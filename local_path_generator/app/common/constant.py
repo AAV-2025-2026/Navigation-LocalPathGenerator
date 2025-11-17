ff10dfe2755699f1d1507c5bb7ca6c5a37a81bfd
ruangfafa <ruangfafa@outlook.com>
2025-11-17 16:16:05 -0500

    [feature]Initial ros2 frame

---
+++ local_path_generator/app/common/constant.py
@@ -0,0 +1,5 @@
+class LogMessageCons:
+    SAMPLE=""
+
+class ApplicationCons:
+    MAIN_MODUAL="__main__"
---
+++ local_path_generator/app/controller/local_path_generator.py
@@ -0,0 +1,6 @@
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class LocalPathGenerator(Node):
+    def __init__(self, logger):
+        return
